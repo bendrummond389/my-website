6ea839ca017becdfc3e9fd239bed56f536c1e2d1
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-03-06 12:55:08 -0600

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import '../app.postcss';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+	} from 'flowbite-svelte';
+	import { writable } from 'svelte/store';
+
+	const activeNavLi = writable(null);
+
+	function handleClick(navLi) {
+		activeNavLi.set(navLi);
+	}
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<span
+			class="whitespace-nowrap text-xl font-semibold dark:text-white"
+		>
+			Ben Drummond
+		</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi
+			href="/"
+			active={$activeNavLi === 'home'}
+			on:click={() => handleClick('home')}
+		>
+			Home
+		</NavLi>
+		<NavLi
+			href="/resume"
+			active={$activeNavLi === 'resume'}
+			on:click={() => handleClick('resume')}
+		>
+			Resume
+		</NavLi>
+		<NavLi
+			href="/blog"
+			active={$activeNavLi === 'blog'}
+			on:click={() => handleClick('blog')}
+		>
+			Blog
+		</NavLi>
+		<NavLi
+			href="/contact"
+			active={$activeNavLi === 'contact'}
+			on:click={() => handleClick('contact')}
+		>
+			Contact
+		</NavLi>
+	</NavUl>
+</Navbar>
+
+<slot />
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import logo from '$lib/assets/bendrummond.png';
+	import { Card } from 'flowbite-svelte';
+</script>
+
+<div class="flex justify-center w-full h-4/5">
+	<Card size="lg" padding='sm' >
+    <img src={logo} />
+  </Card>
+</div>
