057803c5e0790b71809623f51d476f86c2bfde00
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-03-06 15:05:35 -0600

    Changed resume to pdf

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,4 @@
+<!-- Import necessary dependencies -->
 <script lang="ts">
 	import '../app.postcss';
 	import {
@@ -9,13 +10,16 @@
 	} from 'flowbite-svelte';
 	import { writable } from 'svelte/store';
 
+	// Create a writable store to hold the currently active navigation item
 	const activeNavLi = writable(null);
 
+	// Define a function to update the activeNavLi store based on user click
 	function handleClick(navLi) {
 		activeNavLi.set(navLi);
 	}
 </script>
 
+<!-- Define the navbar component with various navigation links -->
 <Navbar let:hidden let:toggle>
 	<NavBrand href="/">
 		<span
@@ -24,8 +28,10 @@
 			Ben Drummond
 		</span>
 	</NavBrand>
+	<!-- Hamburger icon for mobile screens -->
 	<NavHamburger on:click={toggle} />
 	<NavUl {hidden}>
+		<!-- Define the navigation links with their corresponding href, active state, and click function -->
 		<NavLi
 			href="/"
 			active={$activeNavLi === 'home'}
@@ -57,4 +63,5 @@
 	</NavUl>
 </Navbar>
 
+<!-- Render the main content of the website -->
 <slot />
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,10 +1,33 @@
+<!-- Import the resume PDF file using the $lib alias -->
 <script>
-	import logo from '$lib/assets/bendrummond.png';
-	import { Card } from 'flowbite-svelte';
+	import resume from '$lib/assets/bendrummond.pdf';
 </script>
 
-<div class="flex justify-center w-full h-4/5">
-	<Card size="lg" padding='sm' >
-    <img src={logo} />
-  </Card>
+<!-- Create a responsive iframe container to embed the PDF file -->
+<div class="responsive-iframe-container">
+	<!-- Set the source URL of the iframe to the imported PDF file and provide a title -->
+	<iframe
+		src={resume}
+		title="Ben Drummond Resume"
+	/>
 </div>
+
+<!-- Apply styles to the iframe container and iframe to make them responsive -->
+<style>
+	/* Set the container to have a 16:9 aspect ratio and fill the width of its parent */
+	.responsive-iframe-container {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding-bottom: 56.25%; /* This is the aspect ratio of 16:9 */
+	}
+
+	/* Set the iframe to fill the container and remove any extra spacing */
+	.responsive-iframe-container iframe {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
