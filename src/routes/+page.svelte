0e601beb9d2218cc0fc54060d50047d3eb3af39c
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-03-19 18:50:16 -0600

    Added home page (needs styling)
    looks bad but ok for not

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,48 @@
+<!-- Home.svelte -->
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import {
+		Card,
+		Button,
+		P,
+	} from 'flowbite-svelte';
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-	</Alert>
+<div class="paper-container">
+	<h5
+		class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+	>
+		Welcome to my website
+	</h5>
+
+	<P size="lg">
+		My name is Ben and I'm a Full-Stack Developer
+		with experience working on both large-scale
+		and small-scale websites. I specialize in
+		technologies such as Node.js, React,
+		PostgreSQL, and AWS, among others, and I'm
+		committed to delivering high-quality products
+		that meet my clients' needs and requirements.
+		After an initial consultation to discuss
+		project requirements, I conduct research,
+		create a mockup, and develop a functional
+		frontend that matches the mockup. I work
+		closely with my clients to tailor the frontend
+		until it meets their vision, and then move on
+		to the backend development and deployment.
+		Throughout the process, I gather feedback and
+		make necessary updates until my clients are
+		completely satisfied with the final product.
+		<h5
+			class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+		>
+			My interests
+		</h5>
+		In my free time, I pursue my interests in artificial
+		intelligence and embedded systems, which are closely
+		related to Web Development. I also enjoy outdoor
+		activities such as skiing, surfing, camping, and
+		fishing, and spending time with friends. These
+		activities help me to recharge and bring new perspectives
+		to my work.
+	</P>
 </div>
